f515378f190cfc00a0a4b2bf7c248a054e72c45d
Prashant <pralashdho123@gmail.com>
2026-03-13 14:39:41 +0545

    merge(app): to prashant

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -3,6 +3,9 @@ from app.api.routes.auth import router as auth_router
 from app.api.routes.convert import router as convert_router
 from app.api.routes.estimate import router as estimate_router
 from app.api.routes.health import router as health_router
+
+from app import api
+
 api_router = APIRouter()
 api_router.include_router(auth_router)
 api_router.include_router(convert_router)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,70 @@
-import "./App.css";
-import Footer from "./components/Footer/Footer";
-import FullScreen from "./components/FullScreen/FullScreen";
-import Navbar from "./components/Navbar/Navbar";
-import CostEstimation from "./pages/CostEstimation/CostEstimation";
-import EstimateResults from "./pages/Estimateresult/estimate_result";
-import LandingPage from "./pages/LandingPage/LandingPage";
-import Login from "./pages/Login/Login";
-import Result from "./pages/Result/Result";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+import Footer from './components/Footer/Footer';
+import FullScreen from './components/FullScreen/FullScreen';
+import Navbar from './components/Navbar/Navbar';
+import CostEstimation from './pages/CostEstimation/CostEstimation';
+import EstimateResults from './pages/Estimateresult/estimate_result';
+import LandingPage from './pages/LandingPage/LandingPage';
+import Login from './pages/Login/Login';
+import Result from './pages/Result/Result';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route
-          path="/login"
-          element={
-            <>
-              <Login></Login>
-              <Footer></Footer>
-            </>
-          }
-        ></Route>
-        <Route
-          path="/"
-          element={
-            <>
-              <Navbar></Navbar>
-              <Footer></Footer>
-              <Footer></Footer>
-            </>
-          }
-        ></Route>
-        <Route
-          path="/results"
-          element={
-            <>
-              <Navbar></Navbar>
-              <Result></Result>
-              <Footer></Footer>
-            </>
-          }
-        ></Route>
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route
+					path='/login'
+					element={
+						<>
+							<Login></Login>
+							<Footer></Footer>
+						</>
+					}
+				></Route>
+				<Route
+					path='/'
+					element={
+						<>
+							<Navbar></Navbar>
+							<LandingPage></LandingPage>
+							<Footer></Footer>
+						</>
+					}
+				></Route>
+				<Route
+					path='/results'
+					element={
+						<>
+							<Navbar></Navbar>
+							<Result></Result>
+							<Footer></Footer>
+						</>
+					}
+				></Route>
+				<Route
+					path='/cost-estimation'
+					element={
+						<>
+							<Navbar></Navbar>
+							<CostEstimation></CostEstimation>
+							<Footer></Footer>
+						</>
+					}
+				></Route>
+				<Route
+					path='/estimate-results'
+					element={
+						<>
+							<Navbar></Navbar>
+							<EstimateResults></EstimateResults>
+							<Footer></Footer>
+						</>
+					}
+				></Route>
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
